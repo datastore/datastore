76897a864908183d0f7b457c79e7262bc1320aa6
jbenet <jbenet@cs.stanford.edu>
2011-12-18 14:50:14 -0800

    Added LRUCache and test_lrucache.py for a pylru based datastore

---
+++ datastore/impl/lrucache.py
@@ -0,0 +1,44 @@
+
+import basic
+import pylru
+
+__version__ = '1.0'
+__author__ = 'Juan Batiz-Benet <jbenet@cs.stanford.edu>'
+
+
+class LRUCache(basic.Datastore):
+  '''Represents an LRU cache datastore, backed by pylru.'''
+
+  def __init__(self, size):
+    self._cache = pylru.lrucache(size)
+
+  def __len__(self):
+    return len(self._cache)
+
+  def clear(self):
+    self._cache.clear()
+
+  def get(self, key):
+    '''Return the object named by key.'''
+    try:
+      return self._cache[key]
+    except KeyError, e:
+      return None
+
+  def put(self, key, value):
+    '''Stores the object.'''
+    self._cache[key] = value
+
+  def delete(self, key):
+    '''Removes the object.'''
+    if key in self._cache:
+      del self._cache[key]
+
+  def contains(self, key):
+    '''Returns whether the object is in this datastore.'''
+    return key in self._cache
+
+  def query(self, query):
+    '''Returns a sequence of objects matching criteria expressed in `query`'''
+    # entire dataset already in memory, so ok to apply query naively
+    return query(self._cache.values())
---
+++ test/test_lrucache.py
@@ -0,0 +1,50 @@
+
+import unittest
+
+from datastore import Key
+from test_basic import TestDatastore
+
+class TestLRUCacheDatastore(TestDatastore):
+
+  def test_lru(self):
+
+    from dronestore.datastore import lrucache
+
+    lru1 = lrucache.LRUCache(1000)
+    lru2 = lrucache.LRUCache(2000)
+    lru3 = lrucache.LRUCache(3000)
+    lrus = [lru1, lru2, lru3]
+
+    for value in range(0, 3000):
+      key = Key('/LRU/%d' % value)
+      for lru in lrus:
+        self.assertFalse(lru.contains(key))
+
+        lru.put(key, value)
+
+        self.assertTrue(lru.contains(key))
+        self.assertEqual(lru.get(key), value)
+
+    self.assertEqual(len(lru1), 1000)
+    self.assertEqual(len(lru2), 2000)
+    self.assertEqual(len(lru3), 3000)
+
+    for value in range(0, 3000):
+      key = Key('/LRU/%d' % value)
+      self.assertEqual(lru1.contains(key), value >= 2000)
+      self.assertEqual(lru2.contains(key), value >= 1000)
+      self.assertTrue(lru3.contains(key))
+
+    lru1.clear()
+    lru2.clear()
+    lru3.clear()
+
+    self.assertEqual(len(lru1), 0)
+    self.assertEqual(len(lru2), 0)
+    self.assertEqual(len(lru3), 0)
+
+    self.test_simple(lrus)
+
+
+if __name__ == '__main__':
+  unittest.main()
